c637b6f8e452b85399110b0a0fc8ae8694e7a633
Your Name <tkorhonen13@gmail.com>
2022-03-24 20:02:41 +0200

    added: testing to sketch component

--- src/components/Sketch/__test__/Sketch.test.tsx
+++ src/components/Sketch/__test__/Sketch.test.tsx
@@ -3,13 +3,38 @@ import userEvent from "@testing-library/user-event";
 import Sketch from "../Sketch";
 
 describe("Sketch", () => {
-  test("Testing ", () => {
-    render(<Sketch />);
-    const eraserBtn = screen.getByRole("button", {
-      name: /eraser/i,
+  describe("pen testing", () => {
+    test("should be able to add color to grid", () => {
+      render(<Sketch />);
+      const grid = screen.getAllByRole("article");
+      expect(grid[0]).toHaveStyle("background-color: inherit;");
+      userEvent.click(grid[0]);
+      expect(grid[0]).toHaveStyle("background-color: #000000;");
     });
-    // userEvent.click(eraserBtn);
-    userEvent.click(eraserBtn);
-    expect(eraserBtn).toHaveClass("buttonToggled");
+    test("should be able to add color to multiple nodes by dragging", () => {
+      render(<Sketch />);
+      const grid = screen.getAllByRole("article");
+      expect(grid[0]).toHaveStyle("background-color: inherit;");
+      expect(grid[1]).toHaveStyle("background-color: inherit;");
+      expect(grid[2]).toHaveStyle("background-color: inherit;");
+      fireEvent.mouseDown(grid[0]);
+      fireEvent.mouseEnter(grid[1]);
+      fireEvent.mouseEnter(grid[2]);
+      fireEvent.mouseUp(grid[2]);
+      expect(grid[0]).toHaveStyle("background-color: #000000;");
+      expect(grid[1]).toHaveStyle("background-color: #000000;");
+      expect(grid[2]).toHaveStyle("background-color: #000000;");
+    });
+    test("Should not add color after releasing mouse", () => {
+      render(<Sketch />);
+      const grid = screen.getAllByRole("article");
+      expect(grid[0]).toHaveStyle("background-color: inherit;");
+      expect(grid[1]).toHaveStyle("background-color: inherit;");
+      fireEvent.mouseDown(grid[0]);
+      fireEvent.mouseEnter(grid[1]);
+      fireEvent.mouseUp(grid[1]);
+      fireEvent.mouseEnter(grid[2]);
+      expect(grid[2]).not.toHaveStyle("background-color: #000000");
+    });
   });
 });
